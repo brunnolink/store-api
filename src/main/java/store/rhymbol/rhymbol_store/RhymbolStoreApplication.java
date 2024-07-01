94a1c3e41cea2da9eade79aecefd914ef2f80a76
brunno <brunnolinker@gmail.com>
2024-07-01 01:51:16 -0300

    feat: application initialzr

---
+++ src/main/java/store/rhymbol/rhymbol_store/RhymbolStoreApplication.java
@@ -0,0 +1,13 @@
+package store.rhymbol.rhymbol_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhymbolStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhymbolStoreApplication.class, args);
+	}
+
+}
